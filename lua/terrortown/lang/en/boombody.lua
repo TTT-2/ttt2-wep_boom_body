4bcced94e5557c58e5cf794e959479cec1187807
Tim Goll <tim@timgoll.de>
2021-11-26 20:02:57 +0100

    added missing language line

--- lua/terrortown/lang/en/boombody.lua
+++ lua/terrortown/lang/en/boombody.lua
@@ -2,8 +2,10 @@ local L = LANG.GetLanguageTableReference("en")
 
 L["ttt2_weapon_boom_body"] = "Boom Body"
 L["ttt2_weapon_boom_body_desc"] = "The boom body is an explosive corpse that explodes if a player tries to search it. You and your team mates are warned about the fact taht it is a boom body."
+
 L["boom_body_own_ragdoll"] = "This is your own boom body. Safely pick it up by pressing [{walkkey} + {usekey}]."
 L["boom_body_warn_ragdoll"] = "This is a boom body placed by a teammate. Be careful!"
+L["boom_body_no_search"] = "This is a boom body. You can't search it."
 
 L["boom_body_help_msb1"] = "Drops the boom body on the floor"
 
