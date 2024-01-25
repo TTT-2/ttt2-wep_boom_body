a8b9cb13e9345fb28fc3e58b4458a72c32e191f3
EntranceJew <EntranceJew@gmail.com>
2024-01-24 21:50:16 -0600

    moved all them settings

--- gamemodes/terrortown/entities/weapons/weapon_ttt_boom_body/shared.lua
+++ gamemodes/terrortown/entities/weapons/weapon_ttt_boom_body/shared.lua
@@ -308,6 +308,29 @@ else --CLIENT
 			serverConvar = "ttt2_boom_body_spawn_blood",
 			label = "label_boom_body_spawn_blood"
 		})
+
+		form:MakeCheckBox({
+			serverConvar = "ttt2_boom_body_pain_sound",
+			label = "label_boom_body_pain_sound"
+		})
+
+		form:MakeSlider({
+			serverConvar = "ttt2_boom_body_explosion_delay",
+			label = "label_boom_body_explosion_delay",
+			min = 0,
+			max = 5,
+			decimal = 1,
+			default = 0
+		})
+
+		form:MakeSlider({
+			serverConvar = "ttt2_boom_body_explosion_delay_policing",
+			label = "label_boom_body_explosion_delay_policing",
+			min = 0,
+			max = 5,
+			decimal = 1,
+			default = 0
+		})
 	end
 end
 
